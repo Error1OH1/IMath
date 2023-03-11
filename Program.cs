774f68941a55ecea16fbb236369461a12d788f8c
Babb4ge <atwoodaiden1@gmail.com>
2023-03-10 20:14:59 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Math;
+
+namespace IMath
+{
+    public class IMath
+    {
+        public static string ConvertViaMath(double bytes)
+        {
+            string[] sizes = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB" };
+            double size = bytes;
+            int i = 0;
+
+            while (size >= 1024 && i < sizes.Length - 1)
+            {
+                size /= 1024;
+                i++;
+            }
+            return $"{Round(size, 2)}{sizes[i]}";
+        }
+    }
+}
